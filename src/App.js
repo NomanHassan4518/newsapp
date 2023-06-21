c8d15cfe4f6c5d7cec6a79be9ae4e6f145a16596
unknown <aleemsajjad1@gmail.com>
2023-06-21 18:21:54 +0500

    Add newsapp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <Navbar/>
+      </div>
+    )
+  }
+}
